4bed47d8e43ad8a98414898bb859e7db0aee21bf
Radish <maw411@mail.usask.ca>
2020-05-22 21:32:35 -0500

    Add files via upload
    Updates:
    - Must be a much better way to set up the two methods / functions. 
    - One to create the compliment strand.
    - The other to translate the current reading frame.
    - Query: How to translate the different frames from one list.

--- ORF.py
+++ ORF.py
@@ -30,104 +30,181 @@ import sys
 
 # open the file
 try:
-	dna_File = sys.argv[1]
-	dnaSeq = open(dna_File)
+    dna_File = sys.argv[1]
+    dnaSeq = open(dna_File)
 except:
-	dnaSeq = open('test.txt', 'r')
+    dnaSeq = open('test.txt', 'r')
 
 # Initialize variables
+aa = ""
+aa2 = ""
+codonF1 = ""
+codonF2 = ""
+codonR1 = ""
+codonR2 = ""
+seqList = []
+newSeqList = []
 count = 1
-protein = []
+protein = ""
+protF1 = []
+protF2 = []
+protR1 = []
+protR2 = []
 dict = {'TTT': 'F',
-		'TTC': 'F',
-		'TTA': 'L',
-		'TTG': 'L',
-		'TCT': 'S',
-		'TCC': 'S',
-		'TCA': 'S',
-		'TCG': 'S',
-		'TAT': 'Y',
-		'TAC': 'Y',
-		'TAA': 'Stop',
-		'TAG': 'Stop',
-		'TGT': 'C',
-		'TGC': 'C',
-		'TGA': 'Stop',
-		'TGG': 'W',
-		'CTT': 'L',
-		'CTC': 'L',
-		'CTA': 'L',
-		'CTG': 'L',
-		'CCT': 'P',
-		'CCC': 'P',
-		'CCA': 'P',
-		'CCG': 'P',
-		'CAT': 'H',
-		'CAC': 'H',
-		'CAA': 'Q',
-		'CAG': 'Q',
-		'CGT': 'R',
-		'CGC': 'R',
-		'CGA': 'R',
-		'CGG': 'R',
-		'ATT': 'I',
-		'ATC': 'I',
-		'ATA': 'I',
-		'ATG': 'M',
-		'ACT': 'T',
-		'ACC': 'T',
-		'ACA': 'T',
-		'ACG': 'T',
-		'AAT': 'N',
-		'AAC': 'N',
-		'AAA': 'K',
-		'AAG': 'K',
-		'AGT': 'S',
-		'AGC': 'S',
-		'AGA': 'R',
-		'AGG': 'R',
-		'GTT': 'V',
-		'GTC': 'V',
-		'GTA': 'V',
-		'GTG': 'V',
-		'GCT': 'A',
-		'GCC': 'A',
-		'GCA': 'A',
-		'GCG': 'A',
-		'GAT': 'D',
-		'GAC': 'D',
-		'GAA': 'E',
-		'GAG': 'E',
-		'GGT': 'G',
-		'GGC': 'G',
-		'GGA': 'G',
-		'GGG': 'G' }
+        'TTC': 'F',
+        'TTA': 'L',
+        'TTG': 'L',
+        'TCT': 'S',
+        'TCC': 'S',
+        'TCA': 'S',
+        'TCG': 'S',
+        'TAT': 'Y',
+        'TAC': 'Y',
+        'TAA': 'Stop',
+        'TAG': 'Stop',
+        'TGT': 'C',
+        'TGC': 'C',
+        'TGA': 'Stop',
+        'TGG': 'W',
+        'CTT': 'L',
+        'CTC': 'L',
+        'CTA': 'L',
+        'CTG': 'L',
+        'CCT': 'P',
+        'CCC': 'P',
+        'CCA': 'P',
+        'CCG': 'P',
+        'CAT': 'H',
+        'CAC': 'H',
+        'CAA': 'Q',
+        'CAG': 'Q',
+        'CGT': 'R',
+        'CGC': 'R',
+        'CGA': 'R',
+        'CGG': 'R',
+        'ATT': 'I',
+        'ATC': 'I',
+        'ATA': 'I',
+        'ATG': 'M',
+        'ACT': 'T',
+        'ACC': 'T',
+        'ACA': 'T',
+        'ACG': 'T',
+        'AAT': 'N',
+        'AAC': 'N',
+        'AAA': 'K',
+        'AAG': 'K',
+        'AGT': 'S',
+        'AGC': 'S',
+        'AGA': 'R',
+        'AGG': 'R',
+        'GTT': 'V',
+        'GTC': 'V',
+        'GTA': 'V',
+        'GTG': 'V',
+        'GCT': 'A',
+        'GCC': 'A',
+        'GCA': 'A',
+        'GCG': 'A',
+        'GAT': 'D',
+        'GAC': 'D',
+        'GAA': 'E',
+        'GAG': 'E',
+        'GGT': 'G',
+        'GGC': 'G',
+        'GGA': 'G',
+        'GGG': 'G' }
 
 # Loop to parse the DNA Sequence inside a loop to go through gene name text file
 for lines in dnaSeq:
-	for char in range(0,len(lines)-1, 3):
-		codon = lines[char] + lines[char+1] + lines[char+2]
-		count += 1
-		aa = dict[codon]
-		if aa == 'Stop':
-			pass
-		else:
-			protein += aa
+    for char in range(0,len(lines)-1, 3):
+        codonF1 = lines[char] + lines[char+1] + lines[char+2]
+        codonF2 = lines[char+1] + lines[char+2] + lines[char+3]
+        count += 1
+        aa = dict[codonF1]
+        protF1.append(aa)
+        aa2 = dict[codonF2]
+        protF2.append(aa2)
+        if aa == "Stop" | aa2 == "Stop":
+            pass
+        else:
+            protein += aa
 
+# Take the string dnaSeqString and copy the contents into a list called seqList.
+for char in dnaSeqStr:
+    if(char == 'A'):
+        #print('T', end = " ")
+        seqList.append('T')
+    elif(char == 'C'):
+        #print('G', end = " ")
+        seqList.append('G')
+    elif(char == 'G'):
+        #print('C', end = " ")
+        seqList.append('C')
+    elif(char == 'T'):
+        #print('A', end = " ")
+        seqList.append('A')
+print(" ")
+
+# Flip the contents of seqList into newseqList
+newseqList = seqList[::-1]
+
+# Print to see if the lists (seqList, newseqList) are empty.
+print(seqList)
+print(newseqList)
+
+for i in newseqList:
+    codonF1 = lines[char] + lines[char + 1] + lines[char + 2]
+    codonF2 = lines[char + 1] + lines[char + 2] + lines[char + 3]
+    print(codonF1)
+    print(codonF2)
+    print(aa)
+    print(aa2)
+    count += 1
+    aa = dict[codonF1]
+    protF1.append(aa)
+    aa2 = dict[codonF2]
+    protF2.append(aa2)
+    if aa == "Stop" | aa2 == "Stop":
+        pass
+    else:
+        protein += aa
+print(" ")
+
+def compliment(dnaList):
+    seqList = []
+    reverseList = []
+    for char in dnaList:
+        if(char == 'A'):
+            seqList.append('T')
+        elif(char == 'C'):
+            seqList.append('G')
+        elif(char == 'G'):
+            seqList.append('C')
+        elif(char == 'T'):
+            seqList.append('A')
+    print(" ")
+    newseqList = seqList[::-1]
+
+    return reverseList;
+
+def translate(seqList):
+    protList = []
+    F1 = ""
+    aa = ""
+    for char in seqlist:
+        F1 = lines[char] + lines[char + 1] + lines[char + 2]
+        aa = dict[F1]
+        protList.append(aa)
+        if aa == "Stop" | aa2 == "Stop":
+            pass
+        else:
+            protein += aa
+
+# Print the output.
 print("The protein is {0} and the count is {1}".format(protein, count))
-
-# Loop to parse the DNA Sequence inside a loop to go through gene name text file
-for lines in dnaSeq:
-	for char in range(0,len(lines)-1, 3):
-		codon = lines[char] + lines[char+1] + lines[char+2]
-		count += 1
-		aa = dict[codon]
-		if aa == 'Stop':
-			pass
-		else:
-			protein += aa
-
 print("The protein is {0} and the count is {1}".format(protein, count))
 # Print the output.
 
 # Close files to end program
+dnaSeq.close()
